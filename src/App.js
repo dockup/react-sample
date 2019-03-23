5896a3ce2f1a77c5d6154d83878fdc8f040db31b
Emil Soman <emil.soman@gmail.com>
2019-03-23 12:35:22 -0700

    Demo for Furqan

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   render() {
     return (
       <div className="container mt-5" style={{width: '50%'}}>
-        <h2> Dockup Demo (github-branch: master)</h2>
+        <h2> Dockup Demo (github-branch: furqan-demo)</h2>
         <hr />
         <p>
           This stack has got 3 components (containers).
